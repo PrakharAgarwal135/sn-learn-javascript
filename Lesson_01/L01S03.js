5eac869d60302e7916bf0fb631501e3f85f5395c
Prakhar Agarwal <prakharagarwal135@gmail.com>
2025-01-25 14:44:51 +0530

    Update L01S03.js

--- Lesson_01/L01S03.js
+++ Lesson_01/L01S03.js
@@ -1,6 +1,8 @@
 //
 // L01S03 - example client side script
 //
+// go to system definition > client script to run this 
+
 function onLoad() {
 
   alert('Current state value is: ' + g_form.getValue('state'));
